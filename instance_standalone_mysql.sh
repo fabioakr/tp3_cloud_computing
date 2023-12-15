59947678a9da9bb1f979860f9b2c43bf5d631174
Fabio Akira <47678698+fabioakr@users.noreply.github.com>
2023-12-14 20:31:32 -0500

    Trying to automate the testing log to terminal

--- instance_standalone_mysql.sh
+++ instance_standalone_mysql.sh
@@ -28,6 +28,11 @@ apt-get -y install sysbench
 sysbench oltp_read_write --table-size=1000000 --mysql-db=sakila --mysql-user=root prepare
 sysbench oltp_read_write --table-size=1000000 --threads=6 --time=60 --max-requests=0 --mysql-db=sakila --mysql-user=root run
 
+## THIS CODE IS NOT WORKING. SUPPOSED FIRST TO BE COPYING THE FILE TO THE TERMINAL FOR US... ##
+cd /
+cd /var/log
+cat cloud-init-output.log
+
 ## To see if the sakila database was installed ##
 ## USE sakila;
 ## SHOW FULL TABLES;
@@ -43,6 +48,9 @@ sysbench oltp_read_write --table-size=1000000 --threads=6 --time=60 --max-reques
 ## https://www.datacamp.com/tutorial/my-sql-tutorial
 ## https://www.devart.com/dbforge/mysql/studio/how-to-show-all-database-list-in-mysql.html
 ## https://electrictoolbox.com/run-single-mysql-query-command-line/
+
+## Instal nano for debug purposes 
+## sudo apt install nano
 
 ## How to change .pem file permissions
 ## First access the path to the .pem file. Then run the following command:
