aa6144a7aa06afee90e2f38fb8c983d28a9cb695
Fabio Akira <47678698+fabioakr@users.noreply.github.com>
2023-12-28 12:48:39 -0500

    Implements code that sends commands to instances.

--- teste.py
+++ teste.py
@@ -1,89 +1,50 @@
-""" 
-    This is the main file for TP2, for the Advanced Cloud Computing course. 
-"""
-
-## Imports libraries publicly available. ##
-import json
-import sys
-import time
+#import json
+#import sys
+#import time
 import boto3
 
 ## Imports libraries created by us. ##
-from creating_aws_objects import create_keypair, create_security_group, create_instances
-from cleaning import main as cleaning_after_tests
-from workloads import run_workloads
-
-
-def create_workers_file(client, workers):
-    ## uses a wait function on each instance to wait until instance starts running
-    workers_ip_addresses = []
-    for instance in workers:
-        instance.wait_until_running()
-        private_ip = instance.private_ip_address 
-        
-        # instance.reload() has to be done. If not, the public ip will be None
-        # https://stackoverflow.com/questions/64664813/get-the-public-ipv4-address-of-a-newly-created-amazon-ec2-instance-with-boto3
-        instance.reload()  
-
-        public_ip = instance.public_ip_address
-        workers_ip_addresses.append(public_ip)
-
-    containers_json = {}
-
-    ## Creating the notion of two containers located at diffenrent ports
-    ## in each worker and keeping information about them in containers_json
-    for idx, container in enumerate(workers_ip_addresses):
-        containers_json[f'container{idx * 2 + 1}'] = {
-            "ip": container,
-            "port": "8000",
-            "status": "free"
-        }
-        containers_json[f'container{idx * 2 + 2}'] = {
-            "ip": container,
-            "port": "8001",
-            "status": "free"
-        }
-    ## Keeping this information in workers.json file
-    with open("workers.json", "w") as f:
-        f.write(json.dumps(containers_json))
-
-    # insert the file in the instance
-    with open("instance_orchestrator.sh", "r") as f:
-        lines = f.readlines()
-
-    ## Adds the string created in second argument to the array 
-    ## lines as the one before last element
-    lines.insert(-1, "echo '" + json.dumps(containers_json) + "'| tee test.json\n")
-
-    ## Rewriting the instance_orchestrator.sh
-    with open("instance_orchestrator.sh", "w") as f:
-        f.writelines(lines)
-
-
-def obtain_orchestrator_ip(orchestrator):
-    orchestrator.wait_until_running()
-    private_ip = orchestrator.private_ip_address
-
-    ## Once more, orchestrator.reload() is necesarry otherwise the public IP will be None
-    ## https://stackoverflow.com/questions/64664813/get-the-public-ipv4-address-of-a-newly-created-amazon-ec2-instance-with-boto3
-    orchestrator.reload()
-
-    public_ip = orchestrator.public_ip_address
-    return public_ip
-
+from creating_aws_objects import create_keypair, create_security_group, create_instances, create_instance_profiles
+#from cleaning import main as cleaning_after_tests
+#from workloads import run_workloads
 
 def main():
     ## The 'client' variable creates a link to the EC2 service. ##
-    client = boto3.client('ec2', region_name='us-east-1')
+    ##iam_client = boto3.client('iam')
 
-    ## EC2 client. ##
-    ec2 = boto3.resource('ec2', region_name='us-east-1')
+    # Replace 'your_instance_id' and 'your_command' with your actual values
+    instance_id = 'i-02578f18af0decc31'
+    command = 'apt-get update'  # Replace with the command you want to execute
+    #### IDK IF THIS NEEDS SUDO OR NOT. PLS CHECK
 
-    response = client.get_console_output(
-        InstanceId='i-0c1a336e1a8235088'
+    # Create an SSM client
+    ssm_client = boto3.client('ssm')
+
+    # Send the command to the EC2 instance
+    response = ssm_client.send_command(
+        InstanceIds=[instance_id],
+        DocumentName='AWS-RunShellScript',
+        Parameters={'commands': [command]}
     )
 
-    print(response)
+    # Get the command ID for later retrieval
+    command_id = response['Command']['CommandId']
+
+    # Print the command ID
+    print(f"Command ID: {command_id}")
+
+    # Wait for the command to complete
+    waiter = ssm_client.get_waiter('command_executed')
+    waiter.wait(CommandId=command_id, InstanceId=instance_id)
+
+    # Get the command output
+    output = ssm_client.get_command_invocation(
+        CommandId=command_id,
+        InstanceId=instance_id
+    )['StandardOutputContent']
+
+    # Print the command output
+    print(f'Command Output:\n{output}')
 
 
 ##  Takes the program back to main(). ##
