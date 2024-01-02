7fb85fc9b9805446249f1c129db9c59890fe1d08
Fabio Akira <47678698+fabioakr@users.noreply.github.com>
2024-01-01 20:53:04 -0500

    Deletion of unused files from TP2

--- scripts.sh
+++
@@ -1,6 +0,0 @@
-# Image creation using Dockerfile
-docker build -t log8415-aws-tp2 .
-# Create container using image and enviorment variables
-docker run --env-file ./.env --name log8415 log8415-aws-tp2 "$1" "$2"
-# Delete container
-docker rm log8415
